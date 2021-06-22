e8a636b23fa151b1dc647acf383fef0913fc0667
cmaiso <C4m0mill3Fl3uR>
2021-06-22 10:49:43 +0200

    Initialize Functions & SignUp component

---
+++ pages/register.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <SignUp />
+  </div>
+</template>
+
+<script lang="ts">
+import SignUp from '~/components/authentication/SignUp.vue';
+
+export default {
+  components: {
+    SignUp
+  }
+}
+
+
+</script>
+
+<style scoped>
+
+</style>
