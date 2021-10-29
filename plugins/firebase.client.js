4b22ce5f861929dc18b3c44d05fb1aea22d846b4
cmaiso <C4m0mill3Fl3uR>
2021-10-29 22:40:45 +0200

    Hide api keys

--- plugins/firebase.client.js
+++ plugins/firebase.client.js
@@ -5,14 +5,14 @@ import 'firebase/auth';
 export default (context, inject) => {
   // Firebase app config
   const config = {
-    apiKey: 'AIzaSyCl95pHKzYbwIO2QQAwZzoUoUCQxDdzYxY',
-    authDomain: 'yogalib-developpement.firebaseapp.com',
-    databaseURL: 'https://yogalib-developpement.firebaseio.com',
-    projectId: 'yogalib-developpement',
-    storageBucket: 'yogalib-developpement.appspot.com',
-    messagingSenderId: '625633703061',
-    appId: '1:625633703061:web:4bc17c1d2aae489ab3979c',
-    measurementId: 'G-720R56BDYY',
+    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_FIREBASE_APP_ID,
+    measurementId: process.env.VUE_APP_FIREBASE_APP_MEASUREMENT_ID,
   };
 
   // Init our firebase app
